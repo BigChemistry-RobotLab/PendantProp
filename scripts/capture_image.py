55ba428d98d8c9ae24561ec13c2cce02bef47267
PimDankloff <pim.dankloff@ru.nl>
2025-12-08 16:27:28 +0100

    better calibration I

--- scripts/capture_image.py
+++ scripts/capture_image.py
@@ -4,7 +4,7 @@ from pendantprop.hardware.cameras.pd_cam import PendantDropCamera
 
 
 # Create camera instance and start capturing
-simulate = True  # Change to True to use mock camera
+simulate = False  # Change to True to use mock camera
 if simulate:
     from pendantprop.hardware.cameras.pd_cam_mock import MockPendantDropCamera
     print("Using Mock Camera (simulate mode enabled)")
@@ -15,13 +15,15 @@ else:
 pd_cam.start_capture()
 time.sleep(1)  # Wait for first frame
 img = pd_cam.return_image()
+#save img
+cv2.imwrite("captured_image.png", img)
 #show img this particular moment
 if img is not None:
     # Resize to 50% for display
     width = int(img.shape[1] * 0.5)
     height = int(img.shape[0] * 0.5)
     resized = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-    cv2.imshow('Video Stream', resized)
+    cv2.imshow('Camera View', resized)
     cv2.waitKey(0)  # Wait for a key press to close the window
 pd_cam.close_camera()
 cv2.destroyAllWindows()
