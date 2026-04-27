39589514563e5e70f3fe08cd8ab00031bbae2fa7
PimDankloff <pim.dankloff@ru.nl>
2026-04-27 12:50:16 +0200

    calibration quality of life improvement

--- scripts/test_calibration.py
+++ scripts/test_calibration.py
@@ -15,20 +15,21 @@ droplet_manager = DropletManager(
     containers=containers,
 )
 
-source = containers["7A1"]
+source = containers["8A1"]
 source.sample_id = "TestSample001"
 
 # home robot
 config.home()
 
-average_scale = droplet_manager.calibrate(source=source, vol_droplet=19.0, calibration_time=10.0)
+average_scale = droplet_manager.calibrate(source=source, vol_droplet=19.0, calibration_time=60.0)
 config.logger.info(f"Average scale from calibration: {average_scale}")
 
 config.home()
+settings['image_analysis_settings']['scale'] = average_scale
 
 # save meta data
 config.save_layout_final()
-save_settings(settings)
+save_settings(settings, file_path = "config/settings_calibration.json")
 
 # Log the protocol summary at the end
 config.log_protocol_summary()
